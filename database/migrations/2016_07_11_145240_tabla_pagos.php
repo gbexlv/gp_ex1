d7d0604996ec4a1e359b783090123c701761f667
Santiago Scalise <santi.scalise@gmail.com>
2016-07-11 12:01:49 -0300

    modelo + db pagos

---
+++ database/migrations/2016_07_11_145240_tabla_pagos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TablaPagos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->increments('codigopago');
+            $table->float('importe');
+            $table->datetime('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pagos');
+    }
+}
