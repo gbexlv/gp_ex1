51f889217cbbe87a05f10e2108a359c0561b77ef
Santiago Scalise <santi.scalise@gmail.com>
2016-07-11 14:47:25 -0300

    rutas de post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group('usuarios', function() {
+	Route::post('nuevo', 'UsuarioController@postNuevo');
+	Route::post('editar/{id}', 'UsuarioController@postEditar');
+	Route::post('eliminar/{id}', 'UsuarioController@postDelete');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group('favoritos', function() {
+	Route::post('nuevo', 'FavoritoController@postNuevo');
+	Route::post('eliminar/{id}', 'FavoritoController@postDelete');
+});
+
+Route::group('pagos', function() {
+	Route::post('nuevo', 'PagoController@postNuevo');
+	Route::post('editar/{id}', 'PagoController@postEditar');
+	Route::post('eliminar/{id}', 'PagoController@postDelete');
 });
